6c43ace768aa33a1de0f05eb1a677fdeabb37d2d
Henrique <hecaetano1@hotmail.com>
2023-02-03 20:39:42 -0300

    :goal_net: fixing issues

--- src/io/input-services.cpp
+++ src/io/input-services.cpp
@@ -1,5 +1,9 @@
 #include "../../include/io/input-services.hpp"
 #include "../../include/domain/political-party.hpp"
+
+#include "../../include/exceptions/candidates-file-process-exception.hpp"
+#include "../../include/exceptions/votes-file-process-exception.hpp"
+
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -8,30 +12,30 @@
 namespace input_service {
     //public methods=============//
     ifstream create_reading_stream(string file);
-    void process_candidates_file(ifstream buffer, Election election);
+    void process_candidates_file(ifstream uffer, Election election);
     void process_votes_file(ifstream buffer, Election election);
 
     //private methods============//
     bool candidate_is_valid(string cd_cargo, string cd_detalhe_situacao_cand, int type);
-    bool vote_is_valid(string cd_cargo, string nr_votavel, int type);
-    PoliticalParty update_parties(Election election, vector<string> data);
+    bool vote_is_valid(string cd_cago, string nr_voavel, int type);
+    PoliticalParty update_partes(Election elecion, vector<string> data);
     vector<string> input_formatter(string line);
-    void update_candidates(Election election, PoliticalParty party, vector<string> data);
+   void update_cadidates(Election electin, PoliticalParty party, vector<string> data);
     void update_invalid_candidates(Election election, PoliticalParty party, vector<string> data);
     bool is_elected_candidate(string sit);
-    void process_valid_candidates_votes(Election election, vector<string> data);
+   void process_valid_candidates_vots(Election election, vector<string> data);
     void process_invalid_candidates_votes(Election election, vector<string> data);
 
-    //===========================================================================================//
+   //===========================================================================================//
     ifstream create_reading_stream(string file){
         ifstream buffer(file);
-        return buffer;
+       return buffer;
     }
 
     void process_candidates_file(ifstream stream, Election election){
         string current_line;
         vector<string> current_data;
-        PoliticalParty* party;
+        PoliticalParty *party;
 
         try {
             getline(stream, current_line); // elimina a linha de legenda
@@ -43,7 +47,7 @@ namespace input_service {
                 current_data = input_formatter(current_line);
 
                 // Atualizando mapa de partidos
-                party = &update_parties(election, current_data);
+                party = update_parties(election, current_data);
 
                 // caso candidatura válida
                 if (candidate_is_valid(current_data[13], current_data[68], election.get_type())) {
@@ -54,33 +58,55 @@ namespace input_service {
                 // processamento de potenciais votos de legenda)
                 //@todo
                 if (election.get_type() == stoi(current_data[13])
-                        && current_data[67].compareTo("Válido (legenda)") == 0) {
+                        && current_data[67] == "Válido (legenda)") {
                     update_invalid_candidates(election, *party, current_data);
                 }
             }
         } catch (exception& e) {
             cout << "Erro ao ler uma linha do arquivo de candidatos" << endl;
-            throw new CandidatesFileProcessException();
+            throw candidates_file_process_exception();
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
-            throw new CandidatesFileProcessException();
+            throw candidates_file_process_exception();
         }
     }
-    void input_service::process_votes_file(ifstream buffer, Election election){
-        
+    
+    void input_service::process_votes_file(ifstream stream, Election election){
+        string current_line;
+        vector<string> current_data;
+
+       try {
+            getline(stream, current_line); // Removendo linha de cabeçalho
+            while (stream.good()) {
+
+                // Removendo aspas e separando por ";"
+                current_data = input_formatter(current_line);
+
+                // Se o voto é válido, é processado
+                if (vote_is_valid(current_data[17], current_data[19],  election.get_type())) {
+                    process_valid_candidates_votes(election, current_data);
+                    process_invalid_candidates_votes(election, current_data);
+                }
+            }
+        } catch (exception& e) {
+            cout << "Erro ao ler uma linha do arquivo de votos" << endl;
+            throw votes_file_process_exception();
+        } catch (exception& e) {
+            cout << "A string não pode ser convertida em um tipo numérico" << endl;
+            throw new votes_file_process_exception();
+        }
     }
 
     //=========private methods============//
     bool input_service::candidate_is_valid(string cd_cargo, string cd_detalhe_situacao_cand, int type){
         int cdC, cdD;
-
         try {
             cdC = stoi(cd_cargo); 
             cdD = stoi(cd_detalhe_situacao_cand);
         } catch(exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         if ((cdC == type) && (cdD == 2 || cdD == 16))
             return true;
@@ -88,7 +114,6 @@ namespace input_service {
     }
     bool input_service::vote_is_valid(string cd_cargo, string nr_votavel, int type){
         int candidate_code, nrV;
-
         try {
             candidate_code = stoi(cd_cargo);
             nrV = stoi(nr_votavel);
@@ -96,7 +121,7 @@ namespace input_service {
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico"<< endl;
             throw e;
-        }
+       }
 
         if (candidate_code == type && nrV != 95 && nrV != 96 && nrV != 97 && nrV != 98)
             return true;
@@ -104,8 +129,7 @@ namespace input_service {
     }
     PoliticalParty* input_service::update_parties(Election election, vector<string> data){
         int nr_partido, nr_federacao;
-        string sg_partido = data[28];
-
+       string sg_partido = data[28];
 
         try {
             nr_partido = stoi(data[27]);
@@ -113,7 +137,7 @@ namespace input_service {
         } catch (exception& e) {
         cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         // Se o mapa já possui o partido, ele apenas retorna o partido sem criar um novo
         if (election.get_parties_map().count(nr_partido)) {
@@ -123,14 +147,14 @@ namespace input_service {
     }
     vector<string> input_service::input_formatter(string line){
         vector<string> current_data = split(line, ';');
-            for (int i = 0; i <  current_data.size(); i++) {
+           for (int i= 0; i <  current_data.size(); i++) {
                 current_data[i] = removeChar(current_data[i], '\"');
             }
             return current_data;
     }
     void input_service::update_candidates(Election election, PoliticalParty party, vector<string> data){
         int day;
-        int month;
+       int month;
         int year;
         int nr_candidato;
         int cd_genero;
@@ -153,29 +177,27 @@ namespace input_service {
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
     }
     void input_service::update_invalid_candidates(Election election, PoliticalParty party, vector<string> data){
         int nr_candidato;
-
             try {
             nr_candidato = stoi(data[16]);
         } catch (exception& e) {
             cout <<"A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         election.add_legends_candidates_parties(nr_candidato, &party);
     }
     bool input_service::is_elected_candidate(string sit){
         int situation;
-
         try {
             situation = stoi(sit);
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         if (situation == 2 || situation == 3)
             return true;
@@ -183,14 +205,13 @@ namespace input_service {
     }
     void input_service::process_valid_candidates_votes(Election election, vector<string> data){
         int nr_votavel, qt_votos;
-
         try {
             nr_votavel = stoi(data[19]);
             qt_votos = stoi(data[21]);
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         // Se o número é de um candidato, conta como voto nominal
         if (election.get_candidates_map().count(nr_votavel)) {
@@ -207,14 +228,13 @@ namespace input_service {
     }
     void input_service::process_invalid_candidates_votes(Election election, vector<string> data){
         int nr_votavel, qt_votos;
-
         try {
             nr_votavel = stoi(data[19]);
             qt_votos= stoi(data[21]);
         } catch (exception& e) {
             cout << "A string não pode ser convertida em um tipo numérico" << endl;
             throw e;
-        }
+       }
 
         // Se o número do candidato está no mapa de candidatos inválidos, conta como
         // voto de legenda
@@ -227,10 +247,10 @@ namespace input_service {
 
     vector<std::string> input_service::split(const string &input, char delimiter) {
         vector<std::string> result;
-        string token;
-        istringstream stream(input);
+       string token;
+       stringstream stream(input);
 
-        while (std::getline(stream, token, delimiter)) {
+       while (std::getline(stream, token, delimiter)) {
             result.push_back(token);
         }
         return result;
@@ -238,8 +258,7 @@ namespace input_service {
 
     string input_service::removeChar(string input, char character) {
         string result;
-
-        for (char c : input) {
+       for (char c : input) {
             if (c != character) {
                 result += character;
             }
